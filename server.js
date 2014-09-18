faa4c7724432c32e11db5611a3d2447cd96368f6
Maciek Królikowski <mkkrolik@gmail.com>
2014-09-18 02:32:12 +0200

    Added palm model and snake model

--- server.js
+++ server.js
@@ -11,19 +11,3 @@ app.use(express.static(__dirname )); //ustalamy globalna sciezke
 var server = app.listen(port, function() {
 	console.log('listening on port --> %d', server.address().port);
 });
-
-// var app = require('express')(),
-// 	port =  process.env.PORT || 5000;
-
-// app.get('/', function(req, res) {
-// 	// res.sendFile(__dirname + '/index.html');
-// 	res.redirect('/index.html');
-// 	// res.sendFile(path.join(__dirname, '../public', 'index1.html'));
-// 	// console.log(__dirname + '/index.html');
-// });
-
-// app.use(express.static(__dirname + '/public'));
-
-// var server = app.listen(port, function() {
-// 	console.log('listening on port --> %d', server.address().port);
-// });
